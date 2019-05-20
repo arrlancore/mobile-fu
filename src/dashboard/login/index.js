983d3c2097dd18829454c3588b2cb1e2fd885e8c
chris <chris@gmail.com>
2019-05-20 15:59:31 +0700

    fix(login) use hook instead

--- src/dashboard/login/index.js
+++ src/dashboard/login/index.js
@@ -7,6 +7,7 @@ import { Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { func, object, bool, string } from 'prop-types'
 import { isLogin } from 'utils/userData'
+import usePrevious from 'utils/usePrevious'
 import Logo from 'assets/image/logo/logo1x.png'
 import Helmet from 'components/helmet'
 import Button from 'components/button'
@@ -14,125 +15,121 @@ import Input from 'components/input'
 import Layout from 'components/layout'
 import './style.css'
 
-class NormalLoginForm extends React.Component {
-  state = {
-    nextPath: '/home'
-  }
-  handleSubmit = e => {
+
+function LoginPage(props) {
+  const [ nextPath, setnextPath ] = React.useState('/home')
+  const { error, loading, history } = props
+  const prevError = usePrevious(error)
+
+  const handleSubmit = e => {
     e.preventDefault()
-    this.props.form.validateFields((err, values) => {
+    props.form.validateFields((err, values) => {
       if (!err) {
-        this.props.loginUser(values)
+        props.loginUser(values)
       }
     })
-  };
-
-  componentDidMount() {
-    this.setPathRedirect()
   }
 
-  componentDidUpdate(prevProps) {
-    if (this.props.error && prevProps.error !== this.props.error) {
-      message.error(this.props.error)
+  React.useEffect(() => {
+    setPathRedirect()
+    if (error && error !== prevError) {
+      message.error(props.error)
     }
-  }
+  })
 
-  setPathRedirect() {
+  const setPathRedirect = () => {
     const url = new URL(window.location.href)
     const path = url.searchParams.get('from')
     if (path) {
-      this.setState({ nextPath: path })
+      setnextPath(path)
     }
   }
 
-  render() {
-    const { getFieldDecorator } = this.props.form
-    const { history, loading } = this.props
-    return (
-      <Layout>
-        {isLogin() && <Redirect to={this.state.nextPath} />}
-        <div className="root-login">
-          <Helmet>
-            <title>KPI Login</title>
-          </Helmet>
-          <Row
-            type="flex"
-            justify="center"
-            align="middle"
-          >
-            <Col span={6} xl={4}>
-              <div className="logo_company">
-                <img src={Logo} alt="ati-logo" />
-              </div>
-              <Form onSubmit={this.handleSubmit} className="login-form">
-                <label
-                  style={{ marginBottom: '6px' }}
-                  htmlFor="normal_login_userName"
-                >
+  const { getFieldDecorator } = props.form
+  return (
+    <Layout>
+      {isLogin() && <Redirect to={nextPath} />}
+      <div className="root-login">
+        <Helmet>
+          <title>KPI Login</title>
+        </Helmet>
+        <Row
+          type="flex"
+          justify="center"
+          align="middle"
+        >
+          <Col span={6} xl={4}>
+            <div className="logo_company">
+              <img src={Logo} alt="ati-logo" />
+            </div>
+            <Form onSubmit={handleSubmit} className="login-form">
+              <label
+                style={{ marginBottom: '6px' }}
+                htmlFor="normal_login_userName"
+              >
                 Username
-                </label>
-                <Form.Item>
-                  {getFieldDecorator('userName', {
-                    rules: [
-                      { required: true, message: 'Please input your username!', max: 13 }
-                    ]
-                  })(
-                    <Input
-                      prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                      placeholder="01.01.01.1001"
-                    />
-                  )}
-                </Form.Item>
-                <label
-                  style={{ marginBottom: '6px' }}
-                  htmlFor="normal_login_password"
-                >
+              </label>
+              <Form.Item>
+                {getFieldDecorator('userName', {
+                  rules: [
+                    { required: true, message: 'Please input your username!', max: 13 }
+                  ]
+                })(
+                  <Input
+                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                    placeholder="01.01.01.1001"
+                  />
+                )}
+              </Form.Item>
+              <label
+                style={{ marginBottom: '6px' }}
+                htmlFor="normal_login_password"
+              >
                 Password
-                </label>
-                <Form.Item>
-                  {getFieldDecorator('password', {
-                    rules: [
-                      { required: true, message: 'Please input your Password!' }
-                    ]
-                  })(
-                    <Input
-                      prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                      type="password"
-                      placeholder="******"
-                      onPressEnter={this.handleSubmit}
-                    />
-                  )}
-                </Form.Item>
-                <Col span={24}>
-                  <Button loading={loading} type="primary" htmlType="submit">
+              </label>
+              <Form.Item>
+                {getFieldDecorator('password', {
+                  rules: [
+                    { required: true, message: 'Please input your Password!' }
+                  ]
+                })(
+                  <Input
+                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                    type="password"
+                    placeholder="******"
+                    onPressEnter={handleSubmit}
+                  />
+                )}
+              </Form.Item>
+              <Col span={24}>
+                <Button loading={loading} type="primary" htmlType="submit">
                   LOGIN
-                  </Button>
-                  <div
-                    style={{
-                      textAlign: 'center',
-                      color: '#383838',
-                      fontSize: '12px',
-                      margin: ' 15px 0px'
-                    }}
+                </Button>
+                <div
+                  style={{
+                    textAlign: 'center',
+                    color: '#383838',
+                    fontSize: '12px',
+                    margin: ' 15px 0px'
+                  }}
+                >
+                  <span
+                    onClick={() => history.push('/#forget')}
+                    style={{ cursor: 'pointer' }}
                   >
-                    <span
-                      onClick={() => history.push('/#forget')}
-                      style={{ cursor: 'pointer' }}
-                    >
                       Forgot your password ?
-                    </span>
-                  </div>
-                </Col>
-              </Form>
-            </Col>
-          </Row>
-        </div>
-      </Layout>
-    )
-  }
+                  </span>
+                </div>
+              </Col>
+            </Form>
+          </Col>
+        </Row>
+      </div>
+    </Layout>
+  )
 }
 
-NormalLoginForm.propTypes = {
+LoginPage.propTypes = {
   loginUser: func,
   form: object,
   history: object,
@@ -140,8 +137,8 @@ NormalLoginForm.propTypes = {
   error: string
 }
 
-const WrappedNormalLoginForm = Form.create({ name: 'normal_login' })(
-  NormalLoginForm
+const WrappedLoginPage = Form.create({ name: 'normal_login' })(
+  LoginPage
 )
 
 const loadingSelector = createLoadingSelector(['LOGIN'])
@@ -159,7 +156,4 @@ const mapDispatchToProps = dispatch => ({
   loginUser: form => dispatch(actionLogin(form))
 })
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(WrappedNormalLoginForm)
+export default connect(mapStateToProps, mapDispatchToProps)(WrappedLoginPage)
