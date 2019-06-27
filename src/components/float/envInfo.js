50dab7b8877eeb06a12158e5d70b96033f7e6c16
arrlancore <arrlancore@gmail.com>
2019-06-27 10:58:39 +0700

    :Zap: add env information float

---
+++ src/components/float/envInfo.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Tooltip } from 'antd'
+import './style.css'
+
+function EnvInformation() {
+  const [ floatTo, setFloatTo ] = React.useState('float-left')
+  const info = 'kpi-frontend is running on development mode'
+  const changeFloat = () => {
+    const floats = [ 'float-left', 'float-right' ]
+    if (floatTo === floats[0]) {
+      setFloatTo(floats[1])
+    } else {
+      setFloatTo(floats[0])
+    }
+  }
+  return (
+    <Tooltip placement="right" title={info}>
+      <span onClick={changeFloat} className={`float-env-info ${floatTo}`}>DEV</span>
+    </Tooltip>
+  )
+}
+
+export default EnvInformation
