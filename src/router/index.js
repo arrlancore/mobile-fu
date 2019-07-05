78f9ab906c2948f6398c76570422df3f6f063e50
arrlancore <arrlancore@gmail.com>
2019-07-05 15:13:01 +0700

    add lazyload suspense

--- src/router/index.js
+++ src/router/index.js
@@ -1,75 +1,23 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { Switch, BrowserRouter } from 'react-router-dom'
 import ProtectedRoute from './protectedRoute'
 import CustomRoute from './customRoute'
+import LoadingPage from 'components/loader/pageLoad'
+import routes from './routes'
 
-import Login from 'dashboard/login'
-import Home from 'dashboard/home'
-import KpiCalculation from 'dashboard/kpi/calculation'
-import KpiAchivement from 'dashboard/kpi/achivement'
-import Mapping from 'dashboard/kpi/mapping'
-import Administration from 'dashboard/administration'
-import NotFoundPage from 'dashboard/notfound'
-import User from 'dashboard/administration/user'
-
-const routes = [
-  {
-    path: '/',
-    exact: true,
-    component: Login
-  },
-  {
-    path: '/home',
-    exact: true,
-    component: Home,
-    isProtected: true
-  },
-  {
-    path: '/kpi/calculation',
-    exact: true,
-    component: KpiCalculation,
-    isProtected: true
-  },
-  {
-    path: '/kpi/achivement',
-    exact: true,
-    component: KpiAchivement,
-    isProtected: true
-  },
-  {
-    path: '/management/mapping',
-    exact: true,
-    component: Mapping,
-    isProtected: true
-  },
-  {
-    path: '/administration',
-    exact: true,
-    component: Administration,
-    isProtected: true
-  },
-  {
-    path: '/administration/user',
-    exact: true,
-    component: User,
-    isProtected: true
-  },
-  {
-    path: '*',
-    component: NotFoundPage
-  }
-]
 
 function ReactRouter() {
   return (
     <BrowserRouter>
-      <Switch>
-        {routes.map(({ isProtected, ...rest }, i) => (
-          isProtected
-            ? <ProtectedRoute key={i} {...rest} />
-            : <CustomRoute key={i} {...rest} />
-        ))}
-      </Switch>
+      <Suspense fallback={<LoadingPage />}>
+        <Switch>
+          {routes.map(({ isProtected, ...rest }, i) => (
+            isProtected
+              ? <ProtectedRoute key={i} {...rest} />
+              : <CustomRoute key={i} {...rest} />
+          ))}
+        </Switch>
+      </Suspense>
     </BrowserRouter>
   )
 }
---
+++ src/router/routes.js
@@ -0,0 +1,69 @@
+import { lazy } from 'react'
+import Login from 'dashboard/login'
+import NotFoundPage from 'dashboard/notfound'
+
+// only lazyload the pages protected and with header
+const HomePage = lazy(() => (import('dashboard/home')))
+const KpiCalculationPage = lazy(() => (import('dashboard/kpi/calculation')))
+const KpiAchivementPage = lazy(() => (import('dashboard/kpi/achivement')))
+const MappingPage = lazy(() => (import('dashboard/kpi/mapping')))
+const AdministrationPage = lazy(() => (import('dashboard/administration')))
+const UserPage = lazy(() => (import('dashboard/administration/user')))
+const UserViewPage = lazy(() => (import('dashboard/administration/user/view')))
+
+
+const routes = [
+  {
+    path: '/',
+    exact: true,
+    component: Login
+  },
+  {
+    path: '/home',
+    exact: true,
+    component: HomePage,
+    isProtected: true
+  },
+  {
+    path: '/kpi/calculation',
+    exact: true,
+    component: KpiCalculationPage,
+    isProtected: true
+  },
+  {
+    path: '/kpi/achivement',
+    exact: true,
+    component: KpiAchivementPage,
+    isProtected: true
+  },
+  {
+    path: '/management/mapping',
+    exact: true,
+    component: MappingPage,
+    isProtected: true
+  },
+  {
+    path: '/administration',
+    exact: true,
+    component: AdministrationPage,
+    isProtected: true
+  },
+  {
+    path: '/administration/user',
+    exact: true,
+    component: UserPage,
+    isProtected: true
+  },
+  {
+    path: '/administration/user/:id',
+    exact: true,
+    component: UserViewPage,
+    isProtected: true
+  },
+  {
+    path: '*',
+    component: NotFoundPage
+  }
+]
+
+export default routes
